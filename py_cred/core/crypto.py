7397ab7ea673f029782cb6a278f9ca7451366fa2
btmaste <baybella24@gmail.com>
2025-05-28 02:03:16 -0400

    Implement encryption and decryption methods

--- py_cred/core/crypto.py
+++ py_cred/core/crypto.py
@@ -1,7 +1,30 @@
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 import os
 
 class CryptoManager:
-    def __init__(self):
-        self._key = Fernet.generate_key()  # Fixed: made key private
+    def __init__(self, master_key=None):
+        self._master_key = master_key
+        self._salt = os.urandom(16)
+        self._key = self._derive_key()
+
+    def _derive_key(self):
+        if not self._master_key:
+            return Fernet.generate_key()
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self._salt,
+            iterations=100000,
+        )
+        return base64.urlsafe_b64encode(kdf.derive(self._master_key.encode()))
+
+    def encrypt(self, data):
+        f = Fernet(self._key)
+        return f.encrypt(data.encode())
+
+    def decrypt(self, encrypted_data):
+        f = Fernet(self._key)
+        return f.decrypt(encrypted_data).decode()
